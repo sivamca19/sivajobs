d78348d7a04eca4ad9261591caef99176a2c7f6c
siva <siva.mca19@gmail.com>
2012-02-14 12:04:53 +0530

    admin controller error correction

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,6 +1,12 @@
 class Admin::AdminController < ApplicationController
 	before_filter:authenticate_user!
-
+      before_filter:auth
+ def auth
+  @arr=current_user.roles.map(&:name)
+       if !@arr.include?"admin" then
+         redirect_to "/dashboard/index"
+			 end
+			 end
 	def index
 		end
 	
